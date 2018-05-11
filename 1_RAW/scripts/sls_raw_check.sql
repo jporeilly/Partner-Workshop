e6cba36edc4423e01111ed4478d208e31082f176
James O'Reilly <jp.oreilly@hotmail.com>
2018-05-11 08:05:09 -0600

    commit day 2

---
+++ 1_RAW/scripts/sls_raw_check.sql
@@ -0,0 +1,6 @@
+SELECT * FROM sls_raw.customer;
+SELECT * FROM sls_raw.sales_channel;
+SELECT * FROM sls_raw.lens_category;
+SELECT * FROM sls_raw.lens;
+SELECT * FROM sls_raw.store;
+SELECT * FROM sls_raw.sales;
