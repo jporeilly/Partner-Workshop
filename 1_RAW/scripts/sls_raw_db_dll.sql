58d10ce1ca9253e00e17e24e76022bd3913ab36e
James O'Reilly <jp.oreilly@hotmail.com>
2018-09-09 16:37:24 +0100

    updates

--- 1_RAW/scripts/sls_raw_db_dll.sql
+++ 1_RAW/scripts/sls_raw_db_dll.sql
@@ -21,7 +21,7 @@ USE `sls_raw` ;
 DROP TABLE IF EXISTS `sls_raw`.`customer` ;
 
 CREATE TABLE IF NOT EXISTS `sls_raw`.`customer` (
-  `customer_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` INT(10) NOT NULL AUTO_INCREMENT,
   `name` TINYTEXT NULL DEFAULT NULL,
   `first_name` TINYTEXT NULL DEFAULT NULL,
   `last_name` TINYTEXT NULL DEFAULT NULL,
@@ -31,27 +31,7 @@ CREATE TABLE IF NOT EXISTS `sls_raw`.`customer` (
   `last_updated` DATE NULL DEFAULT NULL,
   PRIMARY KEY (`customer_id`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
--- Table `sls_raw`.`lens`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `sls_raw`.`lens` ;
-
-CREATE TABLE IF NOT EXISTS `sls_raw`.`lens` (
-  `lens_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` TINYTEXT NULL DEFAULT NULL,
-  `lens_category_id` INT(11) NOT NULL,
-  `Category` TINYTEXT NULL DEFAULT NULL,
-  `FMin` DOUBLE NULL DEFAULT NULL,
-  `FMax` TINYTEXT NULL DEFAULT NULL,
-  `AMax` DOUBLE NULL DEFAULT NULL,
-  `AMin` TINYTEXT NULL DEFAULT NULL,
-  `FocusMin` TINYTEXT NULL DEFAULT NULL,
-  `Filter` TINYTEXT NULL DEFAULT NULL,
-  PRIMARY KEY (`lens_id`))
-ENGINE = InnoDB
+AUTO_INCREMENT = 26
 DEFAULT CHARACTER SET = latin1;
 
 
@@ -61,27 +41,39 @@ DEFAULT CHARACTER SET = latin1;
 DROP TABLE IF EXISTS `sls_raw`.`lens_category` ;
 
 CREATE TABLE IF NOT EXISTS `sls_raw`.`lens_category` (
-  `lens_category_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `lens_category_id` INT(10) NOT NULL AUTO_INCREMENT,
   `lens_category` TINYTEXT NULL DEFAULT NULL,
+  `count` INT(10) NULL DEFAULT NULL,
   PRIMARY KEY (`lens_category_id`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 10
 DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `sls_raw`.`sales`
+-- Table `sls_raw`.`lens`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `sls_raw`.`sales` ;
+DROP TABLE IF EXISTS `sls_raw`.`lens` ;
 
-CREATE TABLE IF NOT EXISTS `sls_raw`.`sales` (
-  `date` DATE NULL DEFAULT NULL,
-  `customer_id` INT(11) NULL DEFAULT NULL,
-  `lens_id` INT(11) NULL DEFAULT NULL,
-  `sales_channel_id` INT(11) NULL DEFAULT NULL,
-  `amount` DOUBLE NULL DEFAULT NULL,
-  `store_id` INT(11) NULL DEFAULT NULL,
-  `sales_id` INT(11) NOT NULL,
-  PRIMARY KEY (`sales_id`))
+CREATE TABLE IF NOT EXISTS `sls_raw`.`lens` (
+  `lens_id` INT(10) NOT NULL,
+  `name` TINYTEXT NULL DEFAULT NULL,
+  `lens_category_id` INT(10) NOT NULL,
+  `lens_category` TINYTEXT NULL DEFAULT NULL,
+  `fmin` DOUBLE NULL DEFAULT NULL,
+  `fmax` DOUBLE NULL DEFAULT NULL,
+  `amax` DOUBLE NULL DEFAULT NULL,
+  `amin` DOUBLE NULL DEFAULT NULL,
+  `FocusMin` DOUBLE NULL DEFAULT NULL,
+  `Filter` DOUBLE NULL DEFAULT NULL,
+  `count` INT(10) NULL DEFAULT NULL,
+  PRIMARY KEY (`lens_id`),
+  INDEX `fk_lenas_category_idx` (`lens_category_id` ASC),
+  CONSTRAINT `fk_lenas_category`
+    FOREIGN KEY (`lens_category_id`)
+    REFERENCES `sls_raw`.`lens_category` (`lens_category_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
@@ -96,6 +88,7 @@ CREATE TABLE IF NOT EXISTS `sls_raw`.`sales_channel` (
   `sales_channel` TINYTEXT NULL DEFAULT NULL,
   PRIMARY KEY (`sales_channel_id`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 3
 DEFAULT CHARACTER SET = latin1;
 
 
@@ -108,17 +101,52 @@ CREATE TABLE IF NOT EXISTS `sls_raw`.`store` (
   `store_name` TINYTEXT NULL DEFAULT NULL,
   `store_country` TINYTEXT NULL DEFAULT NULL,
   `store_city` TINYTEXT NULL DEFAULT NULL,
-  `store_id` INT(11) NOT NULL,
+  `store_id` INT(10) NOT NULL,
   PRIMARY KEY (`store_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- -----------------------------------------------------
+-- Table `sls_raw`.`sales`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `sls_raw`.`sales` ;
 
-AUTO_INCREMENT = 56
+CREATE TABLE IF NOT EXISTS `sls_raw`.`sales` (
+  `date` DATE NULL DEFAULT NULL,
+  `customer_id` INT(10) NULL DEFAULT NULL,
+  `lens_id` INT(10) NULL DEFAULT NULL,
+  `sales_channel_id` INT(10) NULL DEFAULT NULL,
+  `amount` DOUBLE NULL DEFAULT NULL,
+  `store_id` INT(10) NULL DEFAULT NULL,
+  `sales_id` INT(10) NOT NULL,
+  `channel_count` INT(10) NULL DEFAULT NULL,
+  PRIMARY KEY (`sales_id`),
+  INDEX `fk_customer_idx` (`customer_id` ASC),
+  INDEX `fk_lens_idx` (`lens_id` ASC),
+  INDEX `fk_slaes_channel_idx` (`sales_channel_id` ASC),
+  INDEX `fk_store_idx` (`store_id` ASC),
+  CONSTRAINT `fk_customer`
+    FOREIGN KEY (`customer_id`)
+    REFERENCES `sls_raw`.`customer` (`customer_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_lens`
+    FOREIGN KEY (`lens_id`)
+    REFERENCES `sls_raw`.`lens` (`lens_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_slaes_channel`
+    FOREIGN KEY (`sales_channel_id`)
+    REFERENCES `sls_raw`.`sales_channel` (`sales_channel_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_store`
+    FOREIGN KEY (`store_id`)
+    REFERENCES `sls_raw`.`store` (`store_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
 
