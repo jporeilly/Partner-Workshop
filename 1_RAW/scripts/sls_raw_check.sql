274fa755c7eddfaf1311e55d9465cd1c7b30e09d
James O'Reilly <jp.oreilly@hotmail.com>
2018-11-15 11:39:31 +0000

    Update Partner Program

--- 1_RAW/scripts/sls_raw_check.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM sls_raw.customer;
-SELECT * FROM sls_raw.sales_channel;
-SELECT * FROM sls_raw.lens_category;
-SELECT * FROM sls_raw.lens;
-SELECT * FROM sls_raw.store;
-SELECT * FROM sls_raw.sales;
